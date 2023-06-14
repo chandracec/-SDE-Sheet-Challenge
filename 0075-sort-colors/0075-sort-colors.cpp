286f1fadac5df5ae994d40c04500b7a0c94e9855
CHANDRAKANT TIWARI <50081484+chandracec@users.noreply.github.com>
2023-06-14 17:54:23 +0530

    Update 0075-sort-colors.cpp

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,4 +1,5 @@
-  class Solution {
+ //DNF SIMPLE
+class Solution {
 public:
      void sortColors(vector<int>& nums) {
      int low=0, mid=0, high=nums.size()-1;
@@ -12,4 +13,18 @@ public:
                 swap(nums[mid], nums[high--]);
         }
      }
+};
+//another approach with counting freq
+class Solution {
+public:
+     void sortColors(vector<int>& nums) {
+    int n = nums.size();
+    int freq0 = 0, freq1 = 0, freq2 = 0;
+
+    for (int i = 0; i < n; i++)
+        nums[i] == 0 ? freq0++ : nums[i] == 1 ? freq1++ : freq2++;
+
+    for (int i = 0; i < n; i++)
+        nums[i] = i < freq0 ? 0 : i < freq0 + freq1 ? 1 : 2;
+}
 };
