6eeeae6c5e9032c1a2b03147f277a9b4d5c97ce6
CHANDRAKANT TIWARI <50081484+chandracec@users.noreply.github.com>
2023-06-28 13:31:54 +0530

    Time: 70 ms (72.54%), Space: 44.8 MB (49.09%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let count = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[count]) {
+            count++;
+            nums[count] = nums[i];
+        }
+    }
+    return count + 1;
+};
